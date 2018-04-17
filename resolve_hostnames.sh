fdc58e30cb65c1ff43f8691672b80eb697de0eeb
Evgeny Boger <boger@contactless.ru>
2018-04-18 00:52:00 +0300

    fix cname resolving

--- resolve_hostnames.sh
+++ resolve_hostnames.sh
@@ -2,7 +2,7 @@
 
 while read line
 do
-    curl -s https://dns.google.com/resolve?name=$line | jq '.Answer[].data' | cut -d'"' -f2
+    curl -s https://dns.google.com/resolve?name=$line | jq '.Answer[] | select (.type==1) | .data' | cut -d'"' -f2
 done
 
 
